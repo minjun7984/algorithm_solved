89ae925ab3eca599002b30c539adaae35e2c6b01
jung min jun <alswns7984@naver.com>
2023-07-11 22:10:16 +0900

    카펫

---
+++ programmers/lv2/카펫.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+
+        for(int i = 3; i <= sum; i++) {
+            int row = i;
+            int col = sum / row;
+
+            if(row > col) {
+                continue;
+            }
+
+            if(((row-2) * (col-2)) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+                break;
+            }
+        }
+        return answer;
+    }
+}
