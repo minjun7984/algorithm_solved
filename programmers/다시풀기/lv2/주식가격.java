fffa1acfd6f6a437721aa65d55107cb005288612
jung min jun <alswns7984@naver.com>
2024-07-10 22:14:29 +0900

    주식가격 (stack 활용 풀이)

---
+++ programmers/다시풀기/lv2/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        //stack에 인덱스를 넣자
+        for (int i = 1; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = prices.length - 1 - index;
+        }
+        return answer;
+    }
+}
